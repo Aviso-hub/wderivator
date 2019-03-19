7f87b51d2747fdd7d3bf6d47b8ea2e2463701943
Aviso <avisosec@protonmail.com>
2019-03-19 15:10:13 +0100

    Rework

--- derivator.py
+++ derivator.py
@@ -1,138 +1,152 @@
 #!/usr/bin/env python3
 import argparse
-import threading
-import queue
 import itertools
 import datetime
 import logging
+import multiprocessing
+
+
+def derivate_case(word):
+    """
+    Return a dict containaing all uppercase and lowercase combinations
+    """
+    logging.debug("Adding all uppercase and lowercase combinations for '{}'".format(word))
+    return list(map(''.join, itertools.product(*zip(word.upper(), word.lower()))))
+
+
+def derivate_leet(word):
+    """
+    Return a dict with all leet combinations
+    """
+    leet = {"a": "4", "b": "8", "e": "3", "g": "6", "i": "1", "l": "1", "o": "0", "r": "2", "s": "5", "t": "7", "y": "7", "z": "2"}
+    return [''.join(letters) for letters in itertools.product(*({c, leet.get(c, c)} for c in word))]
+
+
+def derivate_numbers(words):
+    """
+    Add usual numbers to strings
+    """
+    data = []
+
+    for word in words:
+        logging.debug("Adding usual numbers to '{}'".format(word))
+        data.extend([
+            word + "1",
+            word + "123",
+            word + "123456",
+            "1" + word,
+            "123" + word,
+            "123456" + word
+        ])
+
+    return data
+
+
+def derivate_chars(words):
+    """
+    Add usual characters to strings
+    """
+    data = []
+
+    for word in words:
+        logging.debug("Adding usual characters to '{}'".format(word))
+        data.extend([
+            word + "!",
+            word + "@",
+            word + "#",
+            "!" + word,
+            "@" + word,
+            "#" + word
+        ])
+
+    return data
 
 
 class Derivator:
-    def __init__(self, filename, complexmode):
-        self.filename = filename
-        self.complex = complexmode
-        self.leet = {"a": "4", "b": "8", "e": "3", "g": "6", "i": "1", "l": "1", "o": "0", "r": "2", "s": "5", "t": "7", "y": "7", "z": "2"}
-
-    def run(self):
-        """ Loop until queue is empty """
-        while True:
-            self.w = q.get()
-            self.r = []
-            logging.info("Starting derivation for '{}'".format(self.w))
-            self.derivate()
-            self.saveToFile()
-            q.task_done()
-
-    def saveToFile(self):
-        mutex.acquire()
-        try:
-            with open(self.filename, 'a') as f:
-                for item in self.r:
-                    f.write("{}\n".format(item))
-        finally:
-            mutex.release()
+    def __init__(self, threads):
+        self.data = list()
+        self.thrds = list()
+        self.pool = multiprocessing.Pool(threads)
 
     def extend(self, data):
-        self.r.extend(data)
-        self.r = list(set(self.r))
+        self.data.extend(data)
 
-    def derivate(self):
-        self.extend(self.getUpperLower(self.w))
+    def schedule(self, function, args, callback=None):
+        self.thrds.append(self.pool.apply_async(function, args=args, callback=callback))
 
-        if self.complex:
-            self.extend(self.getLeet(self.r))
+    def derivate(self, word, leet=False):
+        self.data = list()
+        logging.info("Starting derivation for '{}'".format(word))
+        self.extend(derivate_case(word))
 
-        for w in self.r:
-            wnumber = self.addUsualNumbers(w)
-            self.extend(wnumber)
-            wchar = self.addSomeChar(w)
-            self.extend(wchar)
+        if leet:
+            self.extend(derivate_leet(word))
 
-            for wc in wchar:
-                self.extend(self.addUsualNumbers(wc))
+        for w in self.data:
+            self.schedule(derivate_numbers, ([w],), callback=self.on_derivate_numbers_finished)
+            self.schedule(derivate_chars, ([w],), callback=self.on_derivate_chars_finished)
 
-            for wn in wnumber:
-                self.extend(self.addSomeChar(wn))
+        for thrd in self.thrds:
+            self.extend(thrd.get())
 
-    def getUpperLower(self, s):
-        """ Return a dict containaing all uppercase and lowercase combinations """
-        return list(map(''.join, itertools.product(*zip(s.upper(), s.lower()))))
+        return list(set(self.data))
 
-    def getLeet(self, s):
-        """ Return a dict with all leet combinations """
-        ret = []
-        for word in s:
-            ret.extend([''.join(letters) for letters in itertools.product(*({c, self.leet.get(c, c)} for c in word))])
-        return ret
+    def on_derivate_numbers_finished(self, data):
+        for w in data:
+            self.schedule(derivate_chars, ([w],))
 
-    def addUsualNumbers(self, s):
-        """ Add some numbers to string """
-        ret = []
-        ret.append(s + "1")
-        ret.append(s + "123")
-        ret.append(s + "123456")
-        ret.append("1" + s)
-        ret.append("123" + s)
-        ret.append("123456" + s)
-        return ret
-
-    def addSomeChar(self, s):
-        """ Add some char """
-        ret = []
-        ret.append(s + "!")
-        ret.append(s + "@")
-        ret.append(s + "#")
-        ret.append("!" + s)
-        ret.append("@" + s)
-        ret.append("#" + s)
-        return ret
+    def on_derivate_chars_finished(self, data):
+        for w in data:
+            self.schedule(derivate_numbers, ([w],))
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-w", "--words", help="Strings to generate wordlist", nargs="+", required=True)
-    parser.add_argument("-t", "--threads", help="Number of threads", default=1, type=int)
     parser.add_argument("-o", "--output", help="File to output wordlist", required=True)
+    parser.add_argument("-t", "--threads", help="Number of threads", default=5, type=int)
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
-    parser.add_argument("-c", "--complex", help="Generate complex passwords", action="store_true")
+    parser.add_argument("-l", "--leet", help="Generate leet passwords", action="store_true")
     parser.add_argument("-q", "--quiet", help="Quiet mode", action="store_true")
     args = parser.parse_args()
 
     if args.quiet:
         loglevel = logging.FATAL
+        logformat = None
     else:
-        loglevel = logging.DEBUG if args.verbose else logging.INFO
+        if args.verbose:
+            loglevel = logging.DEBUG
+            logformat = "%(asctime)s [%(threadName)-12.12s] [%(funcName)-18.18s] [%(levelname)-5.5s] %(message)s"
+        else:
+            loglevel = logging.INFO
+            logformat = "%(asctime)s %(message)s"
 
     logging.basicConfig(
         level=loglevel,
-        format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+        format=logformat,
         handlers=[
             logging.StreamHandler()
         ]
     )
 
     logging.info("----------------------------------------------------------")
-    logging.info("WDerivator v1.0")
+    logging.info("WDerivator v1.1")
     logging.info("Author : Aviso")
-    logging.info("Last update : 12-2016")
+    logging.info("Last update : 03-2019")
     logging.info("GitHub : https://github.com/Aviso-hub/wderivator")
     logging.info("----------------------------------------------------------")
     logging.info("Starting WDerivator with {} threads".format(args.threads))
 
-    q = queue.Queue()
-    mutex = threading.Lock()
-    logging.debug("Initializing queue")
-    [q.put(_) for _ in args.words]
-    logging.debug("Initializing done")
-
     start = datetime.datetime.now()
-    for thrd in range(args.threads):
-        logging.debug("Starting thread {}".format(thrd))
-        derivator = Derivator(args.output, args.complex)
-        t = threading.Thread(target=derivator.run)
-        t.daemon = True
-        t.start()
+    derivator = Derivator(args.threads)
 
-    q.join()
+    for w in args.words:
+        data = derivator.derivate(w, args.leet)
+        with open(args.output, 'a') as f:
+            for item in data:
+                f.write("{}\n".format(item))
+
+        logging.info("{} password derivated.".format(len(data)))
+
     logging.info("WDerivator end (time elapsed {})".format(datetime.datetime.now() - start))
